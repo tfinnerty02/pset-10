30ff5fc3868859cb12dc5317673b33395748af9d
Thomas Finnerty <tfinnerty02@Thomass-MacBook-Pro-2.local>
2020-02-20 09:10:53 -0500

    commit

--- src/Words.java
+++ src/Words.java
@@ -1,4 +1,31 @@
-
 public class Words {
+	
+	public String word;
+	public String partOfSpeech;
+	public String ;
 
+	public String getWord() {
+		return word;
+	}
+	
+	public String getPartOfSpeech() {
+		return partOfSpeech;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + "]";
+	}
 }
