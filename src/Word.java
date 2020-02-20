cce2a4e7be5a36b4849d698e39b3023ddfafc676
Thomas Finnerty <tfinnerty02@Thomass-MacBook-Pro-2.local>
2020-02-20 09:20:57 -0500

    created classes for word and definition

---
+++ src/Word.java
@@ -0,0 +1,22 @@
+
+public class Word {
+
+	private String word;
+	// create instance variable array of definitions and create a getter for it
+
+	private String[] synonyms;
+	private String[] antonyms;
+
+	public String getWord() {
+		return word;
+	}
+
+	public String[] getSynonyms() {
+		return synonyms;
+	}
+
+	public String[] getAntonyms() {
+		return antonyms;
+	}
+
+}
